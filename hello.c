bd616acc8f78e6b50ab0f460ced85ba5d5623fba
Petar Jevtic <petarjevtic02@gmail.com>
2023-01-11 14:00:00 +0100

    Program

--- hello.c
+++ hello.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
-
 int main ()
-
 {
-char str [100];
-int i;
-printf ("Unesi vrednost: ");
-scanf ("%d", &i);
 
-printf ("\nUneo si: %d\n", i);
+int broj;
 
-return 0;
+printf ("Unesite celi broj: ");
+scanf ("%d", &broj);
 
+if (broj < 0)
+ printf ("Broj je negativan.");
 
-
+else 
+ if (broj > 0)
+ printf ("Broj je pozitivan.");
+else
+printf ("Broj je jednak nuli.");
 
 }
